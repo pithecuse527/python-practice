76c1d04ce476d3efd73a2b29eb1a4ef703eb7463
Ji Hong Geun <gihong96@gmail.com>
2018-09-16 17:00:31 +0900

    Test commit.

--- Ch.7/cir.py
+++ Ch.7/cir.py
@@ -5,17 +5,18 @@
 #
 
 
+
 def is_cir(inpt):
 
     tmp = []
     inpt = inpt.lower()     # 모두 소문자로
-    
+
     for x in list(inpt):     # inpt를 리스트로 만들어서 비교.
         if x.isalpha():      # x가 알파벳이면 tmp에 append.
             tmp.append(x)
 
     n = len(tmp)
-    
+
     for i in range(n):
         if tmp[i] != tmp[n-(i+1)]:      # 회문 검사.
             return False
@@ -26,4 +27,3 @@ if is_cir(inpt):
     print("회문입니다.")
 else :
     print("회문이 아닙니다.")
-            
